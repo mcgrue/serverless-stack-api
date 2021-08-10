5faac74a4190e8fa50df8595a54f9357e50f95cf
Ben McGraw <ben.mcgraw@gmail.com>
2021-08-09 19:55:33 -0700

    WIP

--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -6,7 +6,22 @@ export default function handler(lambda) {
       body = await lambda(event, context);
       statusCode = 200;
     } catch (e) {
-      body = { error: e.message };
+      body = { error: e.message, event, 'context' : context };
+
+      if(event.requestContext) {
+        body.requestContext = JSON.stringify(event.requestContext);
+
+        if(event.requestContext.identity) {
+          body.identity = JSON.stringify(event.requestContext.identity);
+        }
+
+        if(event.requestContext.authorizer) {
+          body.authorizer = JSON.stringify(event.requestContext.authorizer);
+        } else {
+          body.authorizer = 'null';
+        }
+      }
+
       statusCode = 500;
     }
 
